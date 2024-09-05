8e842dbe430dee3a6a1ca9c3ad235b21aefd34ed
maoyutofu <maoyutofu@gmail.com>
2024-09-05 10:24:25 +0800

    fix(json): fix json

--- src/view/json.rs
+++ src/view/json.rs
@@ -56,9 +56,9 @@ pub fn Json() -> impl IntoView {
         <section class="my-5">
         <h2 class="my-5">JSON 在线工具</h2>
         <label for="data" class="block mb-2 text-sm font-medium text-gray-900">数据</label>
-        <textarea on:input=input_data id="data" rows="21" class="block p-2.5 w-full text-sm border-none text-white-900 bg-white-50 focus:ring-blue-500 focus:border-blue-500"></textarea>
+        <textarea on:input=input_data prop:value=data id="data" rows="21" class="block p-2.5 w-full text-sm border-none text-white-900 bg-white-50 focus:ring-blue-500 focus:border-blue-500"></textarea>
         <div class="flex mt-5 justify-end gap-1">
-            <button on:click=format type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 py-2.5 me-2 mb-2">格式化校验</button>
+            <button on:click=format type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 py-2.5 me-2 mb-2">格式化</button>
             <button on:click=compress type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 py-2.5 me-2 mb-2">压缩</button>
             <button on:click=transfer type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 py-2.5 me-2 mb-2">转义</button>
             <button on:click=detransfer type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 py-2.5 me-2 mb-2">去转义</button>
